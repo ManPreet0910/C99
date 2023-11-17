3a62905b35f5963ed17da362fed837a98df43471
ManPreet0910 <133527039+ManPreet0910@users.noreply.github.com>
2023-11-16 20:26:16 -0500

    Add files via upload

---
+++ fileOrganizer.py
@@ -0,0 +1,16 @@
+import os
+import shutil
+
+path=input("Enter the name of the directory:")
+list_of_files = os.listdir(str(path))
+
+for files in list_of_files:
+    name,ext=os.path.splitext(files)
+    ext=ext[1:]
+    if ext == " ":
+        continue
+    if os.path.exists(path+"/"+ext):
+        shutil.move(path+"/"+files, path+"/"+ext+"/"+files)
+    else:
+        os.makedirs(path+"/"+ext)  
+        shutil.move(path+"/"+files, path+"/"+ext+"/"+files)
